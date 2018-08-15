f600b9be0e79ae71e82e64a6d5d2f9a5d4375ebb
Victor K <vkiralyf@gmail.com>
2018-08-15 18:35:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
-var chocolateBars=['snickers','hundred','grand','kitkat','skittles']
+var chocolateBars=['snickers','hundred','grand','kitkat','skittles'];
+function addElementToBeginningOfArray(x,y) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(x,y) {
+  
+}
+function addElementToEndOfArray(x,y) {
+  
+}
+function destructivelyAddElementToEndOfArray(x,y) {
+  
+}
